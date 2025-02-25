9d7ff7d6071c0e37bc1a9e09efa56612c4f9d751
PranavGujarathi <pranavggujarathi@gmail.com>
2025-02-25 15:35:06 +0530

    dictionary done

--- session2/quetions/dictionary.py
+++ session2/quetions/dictionary.py
@@ -1,4 +1,4 @@
-
+'''
 arr={}
 arr[1]=1
 arr['1']=2
@@ -7,3 +7,29 @@ sum=0
 for k in arr:
     sum += arr[k]
 print(sum)
+'''
+'''
+my_dict={}
+my_dict[1]=1
+my_dict['1']=2
+my_dict[1.0]=4
+sum=0
+
+for i in my_dict:
+    sum+=my_dict[i]
+
+print(sum)
+
+'''
+
+my_dict={}
+my_dict[(1,2,4)]=8
+my_dict[(4,2,1)]=10
+my_dict[(1,2)]=12
+
+sum=0
+for i in my_dict:
+    sum+=my_dict[i]
+
+print(sum)
+print(my_dict)
